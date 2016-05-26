5fe0528d78f4a6086f1f17628cf1ffa61bfb26bb
mxdebu <mxdebug@gmail.com>
2016-05-26 14:07:10 -0300

    Create install.py

---
+++ install.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import os 
+import sys
+
+
+
+
+
+
+print '''
+######################
+#versão : 1.0#########
+#autor :luiz gustavo##
+#codinome : mx-debug##
+######################
+   	 ''' 
+print '''
+#esse script vai fazer a instalação do metasploit-framework, para poder usar o novo banco de dados do metas exploit###
+#para poder utilizar o banco de dados pro do metasploit-framework (exploit-db)#                                      #
+######################################################################################################################
+'''
+metas = int(raw_input("voce quer iniciar o script s/n"))
+if metas != s
+	print("vamos iniciar o script.......")
+	instalar = int(raw_input("qual a versao que voce quer instalar "))
